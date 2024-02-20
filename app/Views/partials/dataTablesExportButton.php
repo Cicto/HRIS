8a66814f0e62caa65befafad24530abf7a7dab8f
Egie Santos <122517202+Cicto@users.noreply.github.com>
2024-02-20 15:39:44 +0800

    Users Updates

---
+++ app/Views/partials/dataTablesExportButton.php
@@ -0,0 +1,37 @@
+<!--begin::Export dropdown-->
+<button type="button" class="btn btn-primary btn-sm" data-kt-menu-trigger="click" data-kt-menu-placement="bottom-end">
+    <i class="bi bi-cloud-arrow-down-fill fs-2"></i>
+    Export Report
+</button>
+<!--begin::Menu-->
+<div id="dataTableExportMenu" class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-semibold fs-7 w-200px py-4" data-kt-menu="true">
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-export="copy">
+        Copy to clipboard
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-export="excel">
+        Export as Excel
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-export="csv">
+        Export as CSV
+        </a>
+    </div>
+    <!--end::Menu item-->
+    <!--begin::Menu item-->
+    <div class="menu-item px-3">
+        <a href="#" class="menu-link px-3" data-kt-export="pdf">
+        Export as PDF
+        </a>
+    </div>
+    <!--end::Menu item-->
+</div>
+<div id="dataTableButtonsHook" class="d-none"></div>
