bb98570d134e45cbde58d927fb3dc22fe7d059fb
SoonySideUp <cruz.elison24@gmail.com>
2024-02-29 08:42:28 +0800

    departments update 2/29/2024

--- app/Views/departments/departmentEmployees.php
+++ app/Views/departments/departmentEmployees.php
@@ -56,7 +56,7 @@
                        <div class="symbol-label border" style="background-image:url('<?= base_url()?>/public/assets/media/avatars/default-avatar.png')"></div>
                     </div>
                     <div class="d-flex flex-column justify-content-center">
-                        <h5 class="card-title "><?=$employee->firstname?> <?=$employee->lastname?></h5>
+                        <h5 class="card-title text-capitalized"><?=$employee->firstname?> <?=$employee->lastname?></h5>
                         <p class="card-text " style="color:gray"><?=$employee->employment_status?></p>
                     </div>
                 </div>
